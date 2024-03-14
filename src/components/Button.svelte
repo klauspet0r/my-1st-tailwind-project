7e3bff4ec9203f70ab58faa388af8caab1128693
klauspeter <kreuzbuerger@gmail.com>
2024-03-14 20:24:00 +0100

    somestuff, looks nice

--- src/components/Button.svelte
+++ src/components/Button.svelte
@@ -3,6 +3,6 @@
 </script>
 
 <button
-	class="border-2 border-blue-700 rounded-2xl bg-red-500 hover:bg-red-600 active:bg-red-700 text-nowrap m-2 p-2 w-40"
+	class="border-2 border-blue-700 rounded-2xl bg-sky-500 hover:bg-sky-600 active:bg-sky-700 text-nowrap m-2 p-2 w-40"
 	>{buttonLabel}</button
 >
