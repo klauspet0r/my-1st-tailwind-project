909c8c535dd358eb40256a8e1723447252019b50
klauspeter <kreuzbuerger@gmail.com>
2024-03-14 22:48:53 +0100

    some daisyui stuff

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,23 @@
-<button
-	class="inline-block cursor-pointer rounded-md bg-gray-800 px-4 py-3 text-center text-sm font-semibold uppercase text-white transition duration-200 ease-in-out hover:bg-gray-900"
->
-	Button
-</button>
+<div class="navbar bg-primary text-primary-content">
+	<button class="btn btn-ghost text-xl">🤓</button>
+	<button class="btn btn-ghost text-xl">💡</button>
+	<button class="btn btn-ghost text-xl">🔥</button>
+</div>
+<div class="hero min-h-screen" style="background-image: url(/src/assets/globe_east_2048.jpg);">
+	<div class="hero-overlay bg-opacity-60"></div>
+	<div class="card lg:card-side bg-base-100 shadow-xl">
+		<figure>
+			<img
+				src="https://daisyui.com/images/stock/photo-1494232410401-ad00d5433cfa.jpg"
+				alt="Album"
+			/>
+		</figure>
+		<div class="card-body">
+			<h2 class="card-title">Nerds just wanna have fun</h2>
+			<p>Click the button to launch</p>
+			<div class="card-actions justify-end">
+				<button class="btn btn-primary">Launch</button>
+			</div>
+		</div>
+	</div>
+</div>
