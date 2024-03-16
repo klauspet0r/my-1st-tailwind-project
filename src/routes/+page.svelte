fa809da10b4a3378ff6731092d40e04b9dee8ce4
klauspeter <kreuzbuerger@gmail.com>
2024-03-16 12:25:42 +0100

    more pix, fixed carousel not showing all pix

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,15 @@
 <div class="hero min-h-screen" style="background-image: url(/src/assets/IMG_4055.jpeg);">
 	<div class="hero-overlay bg-opacity-30"></div>
 	<div class="carousel w-8/12">
-		<div id="slide1" class="carousel-item relative w-full object-contain">
+		<div id="slide1" class="carousel-item relative w-full">
 			<!-- svelte-ignore a11y-missing-attribute -->
 			<img src="/src/assets/DSC01382.jpg" class="w-full" />
 			<div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-				<a href="#slide4" class="btn btn-circle">❮</a>
+				<a href="#slide6" class="btn btn-circle">❮</a>
 				<a href="#slide2" class="btn btn-circle">❯</a>
 			</div>
 		</div>
-		<div id="slide2" class="carousel-item relative w-full object-contain">
+		<div id="slide2" class="carousel-item relative w-full">
 			<!-- svelte-ignore a11y-missing-attribute -->
 			<img src="/src/assets/DSC01419.jpg" class="w-full" />
 			<div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
@@ -17,7 +17,7 @@
 				<a href="#slide3" class="btn btn-circle">❯</a>
 			</div>
 		</div>
-		<div id="slide3" class="carousel-item relative w-full object-contain">
+		<div id="slide3" class="carousel-item relative w-full">
 			<!-- svelte-ignore a11y-missing-attribute -->
 			<img src="/src/assets/DSC01493.jpg" class="w-full" />
 			<div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
@@ -25,11 +25,27 @@
 				<a href="#slide4" class="btn btn-circle">❯</a>
 			</div>
 		</div>
-		<div id="slide4" class="carousel-item relative w-full object-contain">
+		<div id="slide4" class="carousel-item relative w-full">
 			<!-- svelte-ignore a11y-missing-attribute -->
 			<img src="/src/assets/DSC01509.jpg" class="w-full" />
 			<div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
 				<a href="#slide3" class="btn btn-circle">❮</a>
+				<a href="#slide5" class="btn btn-circle">❯</a>
+			</div>
+		</div>
+		<div id="slide5" class="carousel-item relative w-full">
+			<!-- svelte-ignore a11y-missing-attribute -->
+			<img src="/src/assets/DSC01581.jpg" class="w-full" />
+			<div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+				<a href="#slide4" class="btn btn-circle">❮</a>
+				<a href="#slide6" class="btn btn-circle">❯</a>
+			</div>
+		</div>
+		<div id="slide6" class="carousel-item relative w-full">
+			<!-- svelte-ignore a11y-missing-attribute -->
+			<img src="/src/assets/RandomPix20161-3.jpg" class="w-full" />
+			<div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+				<a href="#slide5" class="btn btn-circle">❮</a>
 				<a href="#slide1" class="btn btn-circle">❯</a>
 			</div>
 		</div>
